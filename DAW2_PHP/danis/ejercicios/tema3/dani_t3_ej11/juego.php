d91c8b127da49e9cca856876d9d439e2f1b839e8
DaniS <DaniS@DGAME.pihole>
2019-11-10 00:29:45 +0100

    Más cambios en el tablero (botones)

--- DAW2_PHP/danis/ejercicios/tema3/dani_t3_ej11/juego.php
+++ DAW2_PHP/danis/ejercicios/tema3/dani_t3_ej11/juego.php
@@ -21,14 +21,59 @@ HTML;
 ?>
 
 
-<?php $totalJugador=0;$totalBanca=0;?>
-<?php foreach ($_SESSION['yo'] as $carta): ?>
-	<?php $totalJugador += $carta->valor ?>
-<?php endforeach;?>
+<?php $totalJugador=0;$totalBanca=0; foreach ($_SESSION['yo'] as $carta){$totalJugador += $carta->valor;} foreach ($_SESSION['banca'] as $carta){$totalBanca += $carta->valor;}?>
+
+<!-- PARTE DE LOS BOTONES PARA JUGAR -->
+        <?php if ($totalJugador <= 7.4):?>
+        
+        	<?php if ($totalBanca==0):?>
+        		<a href="inicializar.php"><button class="boton" disabled>Nuevo juego</button></a>
+                <a href="sacarCarta.php"><button class="boton">Sacar carta</button></a>
+                
+                <?php if (isset($_SESSION['yo']) && $_SESSION['yo'] != []): ?>
+                	<a href="plantarse.php"><button class="boton">Plantarse</button></a>
+                <?php else:?>
+                	<a href="plantarse.php"><button class="boton" disabled>Plantarse</button></a>
+                <?php endif; ?>
+                
+                
+        	<?php elseif (($totalJugador<$totalBanca && $totalBanca<=7.5) || $totalJugador==$totalBanca): ?>
+        		<a href="inicializar.php"><button class="boton">Nuevo juego</button></a>
+        		<a href="sacarCarta.php"><button class="boton" disabled>Sacar carta</button></a>
+        		<a href="plantarse.php"><button class="boton" disabled>Plantarse</button></a>
+        		<h2>HAS PERDIDO</h2>
+        		<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarLost($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas(); /*header('Location: tablero.php')*/;}?>
+        	<?php elseif ($totalJugador<$totalBanca && $totalBanca>7.5): ?>
+        		<a href="inicializar.php"><button class="boton">Nuevo juego</button></a>
+        		<a href="sacarCarta.php"><button class="boton" disabled>Sacar carta</button></a>
+        		<a href="plantarse.php"><button class="boton" disabled>Plantarse</button></a>
+        		<h2>HAS GANADO</h2>
+        		<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarPuntos($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
+        	<?php endif; ?>
+        
+            
+        
+        <?php elseif ($totalJugador==7.5): ?>
+        	<a href="inicializar.php"><button class="boton">Nuevo juego</button></a>
+        	<a href="sacarCarta.php"><button class="boton" disabled>Sacar carta</button></a>
+        	<a href="plantarse.php"><button class="boton" disabled>Plantarse</button></a>
+        	<h2>HAS GANADO</h2>
+        	<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarPuntos($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
+        
+        <?php elseif ($totalJugador > 7.5):?>
+        	<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarLost($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
+            <a href="inicializar.php"><button class="boton">Nuevo juego</button></a>
+            <a href="sacarCarta.php"><button class="boton" disabled>Sacar carta</button></a>
+        	<a href="plantarse.php"><button class="boton" disabled>Plantarse</button></a>
+            <h2>HAS PERDIDO</h2>
+        
+        <?php endif; ?>
+        <!-- FIN PARTE DE LOS BOTONES PARA JUGAR -->
+
 	<?php if(recoger($_SESSION['usuario'], 'vidas')>0):?>
         
         <?php if (isset($_SESSION['yo']) && $_SESSION['yo'] == [] ): ?>
-        (no se ha jugado ninguna carta todavía)
+        <br><br>(no se ha jugado ninguna carta todavía)
         <?php else: ?>
         	<h3>Jugada (Total: <?= $totalJugador ?>)</h3>
         	<table><tr>
@@ -43,12 +88,7 @@ HTML;
         
         <?php if (isset($_SESSION['banca']) && $_SESSION['banca'] != []): ?>
         	
-        	<?php
-        	//$totalBanca=0;
-                foreach ($_SESSION['banca'] as $carta){
-                    $totalBanca += $carta->valor;
-                }
-            ?>
+        	
         	
         	<h3>Jugada de la banca (Total: <?= $totalBanca ?>)</h3>
         	
@@ -63,43 +103,8 @@ HTML;
         <?php endif; ?>
         
         
-        <?php if ($totalJugador <= 7.4):?>
         
-        	<?php if ($totalBanca==0):?>
-        		<br/>
-                <a href="sacarCarta.php"><button>Sacar carta</button></a>
-                
-                <?php if (isset($_SESSION['yo']) && $_SESSION['yo'] != []): ?>
-                	<a href="plantarse.php"><button>Plantarse</button></a>
-                <?php endif; ?>
-                
-                
-        	<?php elseif (($totalJugador<$totalBanca && $totalBanca<=7.5) || $totalJugador==$totalBanca): ?>
-        		<h2>HAS PERDIDO</h2>
-        		<a href="inicializar.php"><button>Nuevo juego</button></a>
-        		<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarLost($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas(); /*header('Location: tablero.php')*/;}?>
-        	<?php elseif ($totalJugador<$totalBanca && $totalBanca>7.5): ?>
-        		<h2>HAS GANADO</h2>
-        		<a href="inicializar.php"><button>Nuevo juego</button></a>
-        		<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarPuntos($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
-        	<?php endif; ?>
         
-            
-        
-        <?php elseif ($totalJugador==7.5): ?>
-        
-        	<h2>HAS GANADO</h2>
-        	<a href="inicializar.php"><button>Nuevo juego</button></a>
-        	<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarPuntos($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
-        
-        <?php elseif ($totalJugador > 7.5):?>
-        
-        	<h2>HAS PERDIDO</h2>
-        	<?php if (isset($_SESSION['jugada']) && $_SESSION['jugada']) {aumentarLost($_SESSION['usuario']); disminuirVidas($_SESSION['usuario']); $_SESSION['jugada']=false; scriptActualizarPuntosVidas();}?>
-            <br/>
-            <a href="inicializar.php"><button>Nuevo juego</button></a>
-        
-        <?php endif; ?>
 	
 	<?php else: ?>
 		¡¡NO TE QUEDAN VIDAS POR HOY!!
--- DAW2_PHP/danis/ejercicios/tema3/dani_t3_ej11/tablero.php
+++ DAW2_PHP/danis/ejercicios/tema3/dani_t3_ej11/tablero.php
@@ -29,7 +29,7 @@ session_start();
     .cajas{
         margin: 20px;
         //top: 50px;
-        //position: relative;
+        position: relative;
         display: inline-block;
         //background-color: yellow;
         vertical-align: top;
@@ -61,31 +61,109 @@ session_start();
     
   </style>
 	<link rel="stylesheet" type="text/css" href="rwd-table.css">
+	<style type="text/css">
+
+	   
+	   .divTitulo{
+	       background-color: #46637f;
+	       color: #fff;
+	       border-radius: 50px;
+	       text-align: center;
+	       padding-left: 20px;
+	       padding-right: 20px;
+	       min-width: 600px;
+	   }
+	   
+	   .divTitulo span{
+	       color: #dd5;
+	   }
+	   
+	   .pagina {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        
+	   
+	   .centrado{
+	       text-align: center;
+	   }
+        
+        .boton {
+        	box-shadow: 0px 1px 0px 0px #fff6af;
+        	background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+        	background-color:#ffec64;
+        	border-radius:6px;
+        	border:1px solid #ffaa22;
+        	display:inline-block;
+        	cursor:pointer;
+        	color:#333333;
+        	font-family:Arial;
+        	font-size:15px;
+        	font-weight:bold;
+        	padding:6px 24px;
+        	text-decoration:none;
+        	text-shadow:0px 1px 0px #ffee66;
+        }
+        .boton:hover {
+        	background:linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+        	background-color:#ffab23;
+        }
+        .boton:active {
+        	position:relative;
+        	top:1px;
+        }
+        
+        .boton:disabled, .boton[disabled]{
+            box-shadow: 0px 1px 0px 0px #899599;
+        	background:linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);
+        	background-color:#ededed;
+        	border-radius:6px;
+        	border:1px solid #d6bcd6;
+        	display:inline-block;
+        	cursor:pointer;
+        	color:#333333;
+        	font-family:Arial;
+        	font-size:15px;
+        	font-weight:bold;
+        	padding:6px 24px;
+        	text-decoration:none;
+        	text-shadow:0px -1px 0px #e1e2ed;
+        }
+
+        
+        	   
+	</style>
 </head>
 
 <body>
-	<div class="cajas">
-		<h2>Bienvenido <?= $_SESSION['name']; ?> a Siete y media</h2>
-		<?php if ($_SESSION['usuario'] == 'test') {
-		    resetVidas('test');
-		}?>
-		<p>5 vidas cada día</p>
-    	<br>
-    	Puntos total: <input type="text" id="puntostotal" value="<?php echo recoger($_SESSION['usuario'], 'points')?>" size="1" readonly>
-    	Vidas hoy: <input type="text" id="vidashoy" value="<?php echo recoger($_SESSION['usuario'], 'vidas')?>" size="1" readonly>
-    	<a href="logout.php"><button>Salir</button></a>
-    	<br>
-    
-    	<div class="juego">
-    		<?php include_once 'juego.php';?>
+	<div class="centrado">
+		<div class="cajas">
+    	
+    		<div class="divTitulo">
+    			<h2>Bienvenido <span><?= $_SESSION['name']; ?>.</span></h2>
+    		</div>
+    		<?php if ($_SESSION['usuario'] == 'test') {
+    		    resetVidas('test');
+    		}?>
+    		<p>5 vidas cada día</p>
+        	<br>
+        	Puntos total: <input type="text" id="puntostotal" value="<?php echo recoger($_SESSION['usuario'], 'points')?>" size="1" readonly>
+        	Vidas hoy: <input type="text" id="vidashoy" value="<?php echo recoger($_SESSION['usuario'], 'vidas')?>" size="1" readonly>
+        	<a href="logout.php"><button>Salir</button></a>
+        	<br>
+        
+        	<div class="juego">
+        		<?php include_once 'juego.php';?>
+        	</div>
     	</div>
-	</div>
-	
-	<div class="cajas">
-		<h2>TOP 10 Mejores jugadores</h2>
-		<?= crearTopBoard(10); ?>
-		<br>
-		<a href="verTodos.php"><button>Ver todos los jugadores</button></a>
+    	
+    	<div class="cajas">
+    		<h2>TOP 10 Mejores jugadores</h2>
+    		<?= crearTopBoard(10); ?>
+    		<br>
+    		<a href="verTodos.php"><button class="boton">Ver todos los jugadores</button></a>
+    	</div>
 	</div>
 
 </body>
