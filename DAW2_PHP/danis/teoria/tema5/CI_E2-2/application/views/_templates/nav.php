cfaf03a87c62e79f2c96c1d7fe3a50bccff6e2d0
DaniS <DaniS@DGAME>
2019-12-24 14:42:49 +0100

    E2-2 mejorado el navbar menu

--- DAW2_PHP/danis/teoria/tema5/CI_E2-2/application/views/_templates/nav.php
+++ DAW2_PHP/danis/teoria/tema5/CI_E2-2/application/views/_templates/nav.php
@@ -1,52 +1,54 @@
 
+<div class="container">
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target="#myNavbar">
+					<span class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?=base_url()?>">C.C.U</a>
+			</div>
 
-<nav class="container navbar navbar-inverse">
-  <div class="navbar-header">
-    <a class="navbar-brand" href="<?=base_url()?>">C.C.U</a>
-  </div>
-  <div class="collapse navbar-collapse" id="myNavbar">
-    <ul class="nav navbar-nav">
-    
-    <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-           COUNTRY<span class="caret"></span>
-        </a>
-        
-		<ul class="dropdown-menu">
-		  <li><a href="<?=base_url()?>country/c">CREATE</a></li>
-		  <li><a href="<?=base_url()?>country/r">LIST</a></li>
-	     </ul>
-      </li>
-      
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-           CITY<span class="caret"></span>
-        </a>
-        
-		<ul class="dropdown-menu">
-		  <li><a href="<?=base_url()?>city/c">CREATE</a></li>
-		  <li><a href="<?=base_url()?>city/r">LIST</a></li>
-	     </ul>
-      </li>
-      
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-           USER<span class="caret"></span>
-        </a>
-        
-		<ul class="dropdown-menu">
-		  <li><a href="<?=base_url()?>user/c">CREATE</a></li>
-		  <li><a href="<?=base_url()?>user/r">LIST</a></li>
-		  <li><a href="<?=base_url()?>user/regTravel">REGISTER TRAVEL</a></li>
-	     </ul>
-      </li>
-    
-    <li><a href="<?= base_url() ?>#">BTN</a></li>
-    
-    </ul>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
 
-  </div>
-</nav>
+					<li class="dropdown"><a class="dropdown-toggle"
+						data-toggle="dropdown" href="#"> COUNTRY<span class="caret"></span>
+					</a>
 
+						<ul class="dropdown-menu">
+							<li><a href="<?=base_url()?>country/c">CREATE</a></li>
+							<li><a href="<?=base_url()?>country/r">LIST</a></li>
+						</ul></li>
+
+					<li class="dropdown"><a class="dropdown-toggle"
+						data-toggle="dropdown" href="#"> CITY<span class="caret"></span>
+					</a>
+
+						<ul class="dropdown-menu">
+							<li><a href="<?=base_url()?>city/c">CREATE</a></li>
+							<li><a href="<?=base_url()?>city/r">LIST</a></li>
+						</ul></li>
+
+					<li class="dropdown"><a class="dropdown-toggle"
+						data-toggle="dropdown" href="#"> USER<span class="caret"></span>
+					</a>
+
+						<ul class="dropdown-menu">
+							<li><a href="<?=base_url()?>user/c">CREATE</a></li>
+							<li><a href="<?=base_url()?>user/r">LIST</a></li>
+							<li><a href="<?=base_url()?>user/regTravel">REGISTER TRAVEL</a></li>
+						</ul></li>
+
+					<li><a href="https://daw2.ga/" target="_blank">DAW2</a></li>
+
+				</ul>
+
+			</div>
+		</div>
+	</nav>
+</div>
 <br>
 
